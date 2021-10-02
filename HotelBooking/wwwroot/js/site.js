c67667dff2eef2dee4037b0e285fd3369715b340
Serhii Buriak <sergeyburiak55@gmail.com>
2021-10-02 13:24:55 +0200

    Added character limit in hotel description

--- HotelBooking/wwwroot/js/site.js
+++ HotelBooking/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let elements = document.getElementsByClassName("card__desc");
+
+for (var i = 0; i < elements.length; i++) {
+    if (elements[i].innerHTML.length > 480) {
+        elements[i].innerHTML = elements[i].innerHTML.substring(0, 475) + " (more inside)";
+    }
+}
